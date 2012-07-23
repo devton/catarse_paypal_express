f8d1c6b0182a129f14f905d2dd8446389a7746d6
Antônio Roberto Silva <forevertonny@gmail.com>
2012-07-23 01:32:04 -0300

    notifications and notifications specs

--- app/controllers/catarse_paypal_express/payment/paypal_express_controller.rb
+++ app/controllers/catarse_paypal_express/payment/paypal_express_controller.rb
@@ -1,6 +1,32 @@
 module CatarsePaypalExpress::Payment
   class PaypalExpressController < ApplicationController
+    before_filter :setup_gateway
+
     def notifications
+      backer = Backer.find params[:id]
+      response = @@gateway.details_for(backer.payment_token)
+      if response.params['transaction_id'] == params['txn_id']
+        backer.confirm! if response.success?
+        render status: 200, nothing: true
+      else
+        render status: 404, nothing: true
+      end
+    rescue
+      render status: 404, nothing: true
+    end
+
+  private
+
+    def setup_gateway
+      if ::Configuration[:paypal_username] and ::Configuration[:paypal_password] and ::Configuration[:paypal_signature]
+        @@gateway ||= ActiveMerchant::Billing::PaypalExpressGateway.new({
+          :login => ::Configuration[:paypal_username],
+          :password => ::Configuration[:paypal_password],
+          :signature => ::Configuration[:paypal_signature]
+        })
+      else
+        puts "[PayPal] An API Certificate or API Signature is required to make requests to PayPal"
+      end
     end
   end
 end
--- spec/controllers/catarse_paypal_express/payment/paypal_express_controller_spec.rb
+++ spec/controllers/catarse_paypal_express/payment/paypal_express_controller_spec.rb
@@ -1,24 +1,37 @@
 require 'spec_helper'
 
 describe CatarsePaypalExpress::Payment::PaypalExpressController do
+  before do
+    Configuration.create!(name: "paypal_username", value: "usertest_api1.teste.com")
+    Configuration.create!(name: "paypal_password", value: "HVN4PQBGZMHKFVGW")
+    Configuration.create!(name: "paypal_signature", value: "AeL-u-Ox.N6Jennvu1G3BcdiTJxQAWdQcjdpLTB9ZaP0-Xuf-U0EQtnS")
+    ActiveMerchant::Billing::PaypalExpressGateway.any_instance.stub(:details_for).and_return({})
+  end
+
   context 'when receive a notification' do
-    before do
-      ActiveMerchant::Billing::PaypalExpressGateway.any_instance.stub(:details_for).and_return({})
-    end
-
-    it 'and not found the backer should return 404' do
-      post :notifications, { id: 1 }
+    it 'and not found the backer, should return 404' do
+      post :notifications, { id: 1, use_route: 'catarse_paypal_express'}
       response.status.should == 404
     end
 
-    it 'and the transaction ID not match should return 404' do
+    it 'and the transaction ID not match, should return 404' do
       backer = Factory(:backer, payment_id: '1234')
-      post :notifications, { id: backer.id, txn_id: 123 }
+      post :notifications, { id: backer.id, txn_id: 123, use_route: 'catarse_paypal_express' }
       response.status.should == 404
     end
 
-    it 'and the transaction ID match should update the payment status' do
-      pending
+    it 'and the transaction ID match, should update the payment status if successful' do
+      success_payment_response = mock()
+      success_payment_response.stubs(:params).returns({ 'transaction_id' => '1234' })
+      success_payment_response.stubs(:success?).returns(true)
+      ActiveMerchant::Billing::PaypalExpressGateway.any_instance.stub(:details_for).and_return(success_payment_response)
+      backer = Factory(:backer, payment_id: '1234', confirmed: false)
+
+      post :notifications, { id: backer.id, txn_id: 1234, use_route: 'catarse_paypal_express' }
+
+      backer.reload
+      response.status.should == 200
+      backer.confirmed.should be_true
     end
   end
 end
