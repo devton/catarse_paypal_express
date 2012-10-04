9e1901cca04bbaead05eb17a28667c049e91b1a1
Diogo Biazus <diogob@gmail.com>
2012-10-04 16:18:29 -0300

    removing accept terms from review

--- app/assets/javascripts/catarse_paypal_express/application.js
+++ app/assets/javascripts/catarse_paypal_express/application.js
@@ -1,9 +1,2 @@
 $(function(){
-  $('#catarse_paypal_express_form form').change(function(event){
-    if($('#catarse_paypal_express_form form input#accept:checked').length > 0) {
-      $('input[type=submit]', $(event.currentTarget)).attr('disabled', false);
-    } else {
-      $('input[type=submit]', $(event.currentTarget)).attr('disabled', true);
-    }
-  });
 });
