5e79e04b8bea2f1b43279ddc0f70c0d559a7be73
Diogo Biazus <diogob@gmail.com>
2012-12-06 14:19:20 -0200

    added skip_before_filter to enable SSL

--- app/controllers/catarse_paypal_express/payment/paypal_express_controller.rb
+++ app/controllers/catarse_paypal_express/payment/paypal_express_controller.rb
@@ -5,6 +5,7 @@ module CatarsePaypalExpress::Payment
     skip_before_filter :verify_authenticity_token, :only => [:notifications]
     skip_before_filter :detect_locale, :only => [:notifications]
     skip_before_filter :set_locale, :only => [:notifications]
+    skip_before_filter :force_http
 
     before_filter :setup_gateway
 
