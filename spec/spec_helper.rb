82a3a4c41a3fbf3cc81072e5ee6a9b57e253ff86
Diogo Biazus <diogob@gmail.com>
2013-06-04 13:43:06 -0300

    removed gems and gitmodules

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../test/dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-#require 'fakeweb'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
@@ -13,14 +12,6 @@ Dir[File.join(ENGINE_RAILS_ROOT, 'spec/support/**/*.rb')].each {|f| require f }
 
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{ENGINE_RAILS_ROOT}/spec/fixtures"
 
@@ -37,29 +28,8 @@ RSpec.configure do |config|
   # Include Engine routes (needed for Controller specs)
   config.include CatarsePaypalExpress::Engine.routes.url_helpers
 
-  # Include Catarse routes
-  config.include Catarse::Application.routes.url_helpers
-
-  config.include Devise::TestHelpers, :type => :controller
-
   config.include FactoryGirl::Syntax::Methods
-
-  config.before(:each) do
-    CatarseMailchimp::API.stubs(:subscribe)
-    CatarseMailchimp::API.stubs(:unsubscribe)
-  end
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before(:each) do
-    Project.any_instance.stub(:store_image_url).and_return('http://www.store_image_url.com')
-  end
 end
-
-RoutingFilter.active = false # Because this issue: https://github.com/svenfuchs/routing-filter/issues/36
 
 def fixture_file(filename)
   return nil if filename.nil?
