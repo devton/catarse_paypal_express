79a978971c097492204a2b8e4f0cf2fda99d26f4
Diogo Biazus <diogob@gmail.com>
2013-06-04 13:44:56 -0300

    added test dummy dir to repository

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '21c461065b81f8bee3308c8773a98feacd4ea509c775b99e402b8bb08bdfc10334b69e2a19ce26ee8b053287947887e6d30b2ac53601fec09db1108d50b34182'
---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+
+  mount CatarseMoip::Engine => "/catarse_moip"
+end
