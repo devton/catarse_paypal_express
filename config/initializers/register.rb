accf2d1ed4686a8704c04936690eccb06466413d
Diogo Biazus <diogob@gmail.com>
2013-06-10 12:51:58 -0300

    fixing review page path during registration of payment engine

--- config/initializers/register.rb
+++ config/initializers/register.rb
@@ -1,5 +1,5 @@
 begin
-  PaymentEngines.register({name: 'paypal', review_path: ->(backer){ CatarsePaypalExpress::Engine.routes.url_helpers.payment_review_paypal_express_path(backer) }, locale: 'en'})
+  PaymentEngines.register({name: 'paypal', review_path: ->(backer){ CatarsePaypalExpress::Engine.routes.url_helpers.review_paypal_expres_path(backer) }, locale: 'en'})
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
