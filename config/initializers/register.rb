88e4c10591ec2a9a5daf46f16550adff46d21d8c
Antônio Roberto Silva <forevertonny@gmail.com>
2014-03-13 15:17:25 -0300

    added can_do_refund? to check if the contribution payment engine can do direct refund

--- config/initializers/register.rb
+++ config/initializers/register.rb
@@ -4,6 +4,7 @@ begin
     review_path: ->(contribution) {
       CatarsePaypalExpress::Engine.routes.url_helpers.review_paypal_express_path(contribution)
     },
+    can_do_refund?: true,
     direct_refund: ->(contribution) {
       CatarsePaypalExpress::ContributionActions.new(contribution).refund
     },
